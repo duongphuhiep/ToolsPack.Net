83bc74cd3c13c10019762e9d50a32504bccfb954
hiep <duongphuhiep@gmail.com>
2015-03-17 23:35:15 +0100

    add DotNet test

---
+++ DiversDoNet.Tests/MemoryCacheTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.Caching;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DiversDoNet.Tests
+{
+	[TestClass]
+	public class MemoryCacheTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var memoryCache = new MemoryCache("Foo");
+			CacheItemPolicy policy = new CacheItemPolicy();
+			memoryCache.AddOrGetExisting("Pop", 123, DateTimeOffset.MaxValue);
+
+		}
+	}
+}
