02c19310d5e1196c9ec7457bd31bcf1f4d5163a1
hiep <duongphuhiep@gmail.com>
2015-03-31 22:36:51 +0200

    add model

---
+++ DiversDoNet.Tests/model/Exposure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DiversDoNet.Tests.model
+{
+	[Serializable]
+	public class Exposure
+	{
+		public virtual string Client { get; set; }
+		public virtual double Amount { get; set; }
+
+		public Exposure()
+		{
+		}
+		public Exposure(string client, double amount)
+		{
+			Client = client;
+			Amount = amount;
+		}
+		public override string ToString()
+		{
+			return string.Format("(Exposure {0} {1})", Client, Amount);
+		}
+	}
+}
