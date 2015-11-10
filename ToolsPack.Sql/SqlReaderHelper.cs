528e65472148f4063b3a2198e7a9bcb694e0f8b9
duongphuhiep <duongphuhiep@gmail.com>
2015-11-10 10:05:35 +0100

    rename

--- ToolsPack.Sql/SqlReaderHelper.cs
+++ ToolsPack.Sql/SqlReaderHelper.cs
@@ -16,41 +16,41 @@ namespace ToolsPack.Sql
 	/// </summary>
 	public static class SqlReaderHelper
 	{
-		private static bool IsNullableType(Type theValueType)
+		private static bool IsNullableType(Type valueType)
 		{
-			return (theValueType.IsGenericType && theValueType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)));
+			return (valueType.IsGenericType && valueType.GetGenericTypeDefinition().Equals(typeof(Nullable<>)));
 		}
 
 		/// <summary>
 		/// Returns the value, of type T, from the SqlDataReader, accounting for both generic and non-generic types.
 		/// </summary>
 		/// <typeparam name="T">T, type applied</typeparam>
-		/// <param name="theReader">The SqlDataReader object that queried the database</param>
-		/// <param name="theColumnName">The column of data to retrieve a value from</param>
+		/// <param name="reader">The SqlDataReader object that queried the database</param>
+		/// <param name="columnName">The column of data to retrieve a value from</param>
 		/// <returns>T, type applied; default value of type if database value is null</returns>
-		public static T GetValue<T>(this SqlDataReader theReader, string theColumnName)
+		public static T GetValue<T>(this SqlDataReader reader, string columnName)
 		{
 			// Read the value out of the reader by string (column name); returns object
-			object theValue = theReader[theColumnName];
+			object v = reader[columnName];
 
 			// Cast to the generic type applied to this method (i.e. int?)
-			Type theValueType = typeof(T);
+			Type valueType = typeof(T);
 
 			// Check for null value from the database
-			if (DBNull.Value != theValue)
+			if (DBNull.Value != v)
 			{
 				// We have a null, do we have a nullable type for T?
-				if (!IsNullableType(theValueType))
+				if (!IsNullableType(valueType))
 				{
 					// No, this is not a nullable type so just change the value's type from object to T
-					return (T)Convert.ChangeType(theValue, theValueType);
+					return (T)Convert.ChangeType(v, valueType);
 				}
 				else
 				{
 					// Yes, this is a nullable type so change the value's type from object to the underlying type of T
-					NullableConverter theNullableConverter = new NullableConverter(theValueType);
+					NullableConverter nullableConverter = new NullableConverter(valueType);
 
-					return (T)Convert.ChangeType(theValue, theNullableConverter.UnderlyingType);
+					return (T)Convert.ChangeType(v, nullableConverter.UnderlyingType);
 				}
 			}
 
