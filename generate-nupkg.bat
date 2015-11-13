3271cd6eb16d3404badd16b761c7668fa1211bc2
duongphuhiep <duongphuhiep@gmail.com>
2015-11-13 12:13:06 +0100

    add some experiment nuspec

---
+++ generate-nupkg.bat
@@ -0,0 +1,3 @@
+nuget pack ToolsPack.Log4net\ToolsPack.Log4net.csproj -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack ToolsPack.Displayer\ToolsPack.Displayer.csproj -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack ToolsPack.Sql\ToolsPack.Sql.csproj -IncludeReferencedProjects -Prop Configuration=Release
