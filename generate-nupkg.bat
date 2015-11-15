a8f0d905eff3e7f49f817f3e650de80b8de6e7b6
duongphuhiep <duongphuhiep@gmail.com>
2015-11-16 00:41:29 +0100

    add pkg

--- generate-nupkg.bat
+++ generate-nupkg.bat
@@ -1,4 +1,4 @@
 nuget pack ToolsPack.Log4net\ToolsPack.Log4net.csproj -IncludeReferencedProjects -Prop Configuration=Release
 nuget pack ToolsPack.Displayer\ToolsPack.Displayer.csproj -IncludeReferencedProjects -Prop Configuration=Release
 nuget pack ToolsPack.Sql\ToolsPack.Sql.csproj -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack ToolsPack.Sql\ToolsPack.Thread.csproj -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack ToolsPack.Thread\ToolsPack.Thread.csproj -IncludeReferencedProjects -Prop Configuration=Release
